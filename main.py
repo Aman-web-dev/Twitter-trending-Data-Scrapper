bb6f5f7f5e1a748ce36fe3ce20cbfe0e1472b788
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-12-27 22:41:57 +0530

    Driver error fixed

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from pymongo import MongoClient
 from datetime import datetime
 from selenium.webdriver.support.ui import WebDriverWait
